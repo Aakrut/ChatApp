5100db343d2c2cb73934504a6cd2942bf23e04dc
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-12-03 15:43:10 +0530

    chatapp

--- app/src/main/java/com/ex/chatappfirebase/MainActivity.kt
+++ app/src/main/java/com/ex/chatappfirebase/MainActivity.kt
@@ -2,10 +2,37 @@ package com.ex.chatappfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.ex.chatappfirebase.databinding.ActivityMainBinding
+import com.ex.chatappfirebase.fragments.HomeFragment
+import com.ex.chatappfirebase.fragments.ProfileFragment
+import com.ex.chatappfirebase.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        loadFragment(HomeFragment())
+
+        bottomNav()
+
+    }
+
+    private fun bottomNav() {
+     
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
